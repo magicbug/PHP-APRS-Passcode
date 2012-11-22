9676994bab90abeabd48e0167a1f822d010ecc2b
Peter Goodhall <peter@magicbug.co.uk>
2012-11-22 21:31:07 +0000

    Added code comments

--- index.php
+++ index.php
@@ -6,15 +6,23 @@
 	$app = new Slim();
 
 	// Functions
+	
 	//GET route
 	$app->get('/', function () {
+		
 		// Require HTML Template
 		require('templates/main.php');
 	});
 	
+	// Generate Passcode
 	$app->post('/passcode', function () {
+		// Include function to generate APRS Code
 		require('aprs_func.php');
+		
+		// Store Passcode
 		$passcode = aprspass($_POST['callsign']);
+		
+		// Load Page
 		require('templates/passcode.php');
 	});
 
